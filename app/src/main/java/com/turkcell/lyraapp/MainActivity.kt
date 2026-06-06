e070468794979211184f8761ff789ff27bd584db
melikesukocyigit <melikesukocyigt@hotmail.com>
2026-06-06 11:16:53 +0300

    dosya isimleri değiştirildi

--- app/src/main/java/com/turkcell/lyraapp/MainActivity.kt
+++ app/src/main/java/com/turkcell/lyraapp/MainActivity.kt
@@ -10,15 +10,16 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.turkcell.lyraapp.ui.theme.LyraTheme
+import com.turkcell.lyraapp.ui.theme.LyraAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LyraTheme {
+            LyraAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -34,14 +35,14 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    LyraTheme {
+    LyraAppTheme {
         Greeting("Android")
     }
 }
