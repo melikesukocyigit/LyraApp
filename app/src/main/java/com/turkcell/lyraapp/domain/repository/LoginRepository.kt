290af0733de7b8cfc5c7d1629ba85c5e76bb5187
melikesukocyigit <melikesukocyigt@hotmail.com>
2026-06-13 01:06:02 +0300

    Kullanılmayan eski repository ve ölü kodlar temizlendi

--- app/src/main/java/com/turkcell/lyraapp/domain/repository/LoginRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.turkcell.lyraapp.domain.repository
-
-/**
- * Login işlemlerinden sorumlu repository arayüzü.
- */
-interface LoginRepository {
-    suspend fun login(phoneNumber: String, password: String): Result<Unit>
-}
